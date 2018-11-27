9c69da3d7ab0ea9f2a022e1638669bd1c59f24ec
Genevieve Hoffman <genny.hoffman@gmail.com>
2018-11-27 15:56:48 -0500

    updates to class code

--- section-4/01-p5-d3-bubbleplot/sketch.js
+++ section-4/01-p5-d3-bubbleplot/sketch.js
@@ -8,9 +8,8 @@ function setup() {
 	h = height - 2 * margin;
 
 	//load data with d3, use data within the anonymous callback function
-	d3.csv('../data/worldwide-health-lifeexpectancy-cut.csv', function(error, data) {
-		if (error) throw error;
-		console.log(data);
+	d3.csv('../data/worldwide-health-lifeexpectancy-cut.csv').then( data => {
+		console.log(data)
 		// d3 map functions are useful for creating more convenient data structures
 		// or applying transformations to values
 		const mappedData = data.map( function(d) {
@@ -20,16 +19,16 @@ function setup() {
 				health: d["2014 Health expenditure, total (% of GDP)"],
 				life: d["2014 Life expectancy at birth, total (years)"],
 				population: d["2014 Population, total"]
-			};
-		});
+			}
+		})
 		console.log(mappedData)
 
 		// filter out rows with fields that don't contain all data fields
 		const filteredData = mappedData.filter( function(d) {
 			if (d.health != "" && d.life != "" && d.population != "") {
-				return d;
+				return d
 			}
-		});
+		})
 		console.log(filteredData)
 
 		// accessor functions which define how to get access to x, y, size and Data values, 
@@ -39,13 +38,13 @@ function setup() {
 		// use the + sign to make sure the data is interpreted as a number, not a string
 		const popData = function(d) {
 			return +d.population
-		};
+		}
 		const healthData = function(d) {
 			return +d.health
-		};
+		}
 		const lifeData = function(d) {
 			return +d.life
-		};
+		}
 
 		console.log(d3.extent(filteredData, popData))
 		console.log(d3.extent(filteredData, healthData))
@@ -66,17 +65,17 @@ function setup() {
 		const sizeScale = d3.scalePow()
 			.exponent(0.5)
 			.domain([d3.min(filteredData, popData), d3.max(filteredData, popData)])
-			.range([10, 100]);
+			.range([10, 100])
 
 		// create canvas
-		createCanvas(width, height);
-		background(255);
+		createCanvas(width, height)
+		background(255)
 
 		// create circles
 		for (let i = 0; i < filteredData.length; i++) {
-			const d = filteredData[i];
-			fill(255, 0, 255, 50);
-			noStroke();
+			const d = filteredData[i]
+			fill(255, 0, 255, 50)
+			noStroke()
 			const c = new Country(
 				d.name,
 				xScale(lifeData(d)),
@@ -84,24 +83,24 @@ function setup() {
 				sizeScale(popData(d))
 			);
 			// add countries to array
-			countries.push(c);
+			countries.push(c)
 			//console.log(xScale(lifeData(d)), yScale(healthData(d)), sizeScale(popData(d)));
 		}
 	});
 }
 
 function draw() {
-	background(255);
+	background(255)
 	// draw circles
 	countries.forEach( function(c) {
-		c.display();
-	});
+		c.display()
+	})
 }
 
 function Country(name, x, y, size) {
-	this.name = name;
-	this.pos = createVector(x, y);
-	this.size = size;
+	this.name = name
+	this.pos = createVector(x, y)
+	this.size = size
 
 	this.display = function() {
 		if (this.isMouseOverCircle()) {
@@ -120,12 +119,12 @@ function Country(name, x, y, size) {
 	}
 
 	this.isMouseOverCircle = function() {
-		let distX = this.pos.x - mouseX;
-		let distY = this.pos.y - mouseY;
+		let distX = this.pos.x - mouseX
+		let distY = this.pos.y - mouseY
 		if (sqrt(sq(distX) + sq(distY)) < this.size / 2) {
-			return true;
+			return true
 		} else {
-			return false;
+			return false
 		}
 	}
 }
